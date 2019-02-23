22a763b2f93c175d3ade621ac2e5375b382d417b
KarateMaltLiquor <43533814+karatemaltliquor@users.noreply.github.com>
2019-02-22 19:15:42 -0500

    latest version

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+//create array of default friends
+
+var friends = [{
+    name: "DMX",
+    photo: "https://images.tmz.com/2018/04/02/0402-dmx-tmz-01-400x300.jpg",
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+  }, {
+    name: "Snoop Dogg",
+    photo: "https://s3.amazonaws.com/hiphopdx-production/2016/03/Snoop-Dogg-Eyes-Closed-e1458497815834-768x620.png",
+    scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+  }, {
+    name: "Riff Raff",
+    photo: "http://stoneyroads.com/wp-content/uploads/2017/06/riffraff-480x320.jpg",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+  },{
+    name: "Bushwick Bill",
+    photo: "https://images.sk-static.com/images/media/profile_images/artists/453448/huge_avatar",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+  }, {
+    name: "Action Bronson",
+    photo: "http://districtmagazine.ie/wp-content/uploads/2018/04/action-bronson-gq-1-web.jpg",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+  }, {
+    name: "The Gieco Gecko",
+    photo: "https://media1.tenor.com/images/15ee7783d28cc72a5d1873eab08eb4a1/tenor.gif?itemid=10854490",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+  }, {
+    name: "Tyler The Creator",
+    photo: "https://miamimusicbuzz.com/wp-content/uploads/2018/01/Tyler-The-Creator-Joins-Kali-Uchis-in-x27x27After-the-Stormx27x27-Video.jpg",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+  }];
+  
+   //export array
+  
+  module.exports = friends;
